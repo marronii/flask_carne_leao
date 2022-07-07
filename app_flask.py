62861020fdb8935bbcc0b9cfc6f37197ff714ced
rafael@rafael.com <96892072+marronii@users.noreply.github.com>
2022-07-06 22:47:10 -0300

    Criado Estrutura tela de login e rota

--- app_flask.py
+++ app_flask.py
@@ -1,2 +1,26 @@
 import pandas as pd
-from flask import render_template
+from flask import render_template, Flask
+
+
+class Cria_Acesso():
+    def __init__(self, nome, senha):
+        self.nome = nome
+        self.senha= senha
+
+
+usuario = Cria_Acesso("Rafael","1234")
+
+
+class Portfolio():
+    pass
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def login():
+    return render_template("login.html")
+
+
+app.run(debug=True)
